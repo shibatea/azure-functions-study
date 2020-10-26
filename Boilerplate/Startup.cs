9829d6100c544e96ca789ec2c9e7a51cc4c8bac4
shibatea <31196732+shibatea@users.noreply.github.com>
2020-10-27 06:16:07 +0900

    Initial commit

---
+++ Boilerplate/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Azure.Identity;
+
+using Boilerplate.Options;
+using Boilerplate.Services;
+
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Boilerplate.Startup))]
+
+namespace Boilerplate
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var context = builder.GetContext();
+
+            builder.Services.AddHttpClient();
+
+            builder.Services.AddSingleton<IGreetingService, GreetingService>();
+            builder.Services.AddSingleton<IHttpService, HttpService>();
+
+            builder.Services.AddSingleton(provider => new CosmosClient(context.Configuration["CosmosConnection"], new CosmosClientOptions
+            {
+                SerializerOptions = new CosmosSerializationOptions
+                {
+                    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+                }
+            }));
+
+            builder.Services.Configure<GreetingOptions>(context.Configuration.GetSection("Greeting"));
+        }
+
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            var context = builder.GetContext();
+
+            if (string.Equals(context.EnvironmentName, "Production", StringComparison.OrdinalIgnoreCase))
+            {
+                // For Production (Using Key Vault with Managed Identity)
+                var builtConfig = builder.ConfigurationBuilder.Build();
+
+                builder.ConfigurationBuilder.AddAzureKeyVault(new Uri(builtConfig["KeyVaultEndpoint"]), new DefaultAzureCredential());
+            }
+            else
+            {
+                // For Local development (Using User Secrets)
+                builder.ConfigurationBuilder.AddUserSecrets<Startup>();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Azure Functions Boilerplate
+
+![Build](https://github.com/shibayan/azure-functions-boilerplate/workflows/Build/badge.svg)
+[![Build Status](https://dev.azure.com/shibayan/azure-functions-boilerplate/_apis/build/status/Build%20azure-functions-boilerplate?branchName=master)](https://dev.azure.com/shibayan/azure-functions-boilerplate/_build/latest?definitionId=44&branchName=master)
+
+This is a quick boilerplate for getting started with Azure Functions v3 and Dependency Injection.
+
+## Goals
+
+1. Dependency Injection ALL THE THINGS
+2. No more need `AccessKey` and `ConnectionString`
+3. Smart configuration / options (Configuration as a Code)
+4. Better cancellation support (Graceful shutdown)
+5. Easier deployment template (CI/CD)
+
+## License
+
+This project is licensed under the [MIT License](https://github.com/shibayan/azure-functions-boilerplate/blob/master/LICENSE)
